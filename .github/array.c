54c9558d28c6e2f4513c546cbd2b3a47b0f92b03
preeti-newcode <preetiraj823@gmail.com>
2025-04-20 13:37:17 +0530

    array.c
    This program allows the user to perform five basic operations on a one-dimensional array. First, it lets the user insert numbers into the array until it becomes full. If the array is full, it will show a message. The user can also delete a specific number from the array, and the program will shift the remaining elements to maintain the order. Another feature is traversal, which displays all the current elements in the array. Additionally, the user can view the array in reverse order. Lastly, the program provides an option to merge the original array with another array of three elements and then displays the combined result. All these operations are controlled through a menu that the user can repeat as many times as they want.

--- .github/array.c
+++ .github/array.c
@@ -43,6 +43,87 @@ void delete(int data){
     }
 }
 
+void traverse(){
+    if(place==-1){
+        printf("\nIts Empty");
+    }else{
+        for(int x=0;x<=place;x++){
+            printf("\n%d  ",array[x]);
+        }
+    }
+}
+
+void reverse(){
+    int reArr[place];
+    printf("\nReverse Array");
+    for(int x=0,y=place;x<=place;x++,y--){
+        reArr[x]=array[y];
+        printf("\n%d    ",reArr[x]);
+    }
+}
+
+void merge(int arr[],int size,int arr2[],int size2){
+    int new[size+size2];
+
+    for(int x=0;x<size;x++){
+        new[x]=arr[x];
+    }
+    for(int x=0;x<size2;x++){
+        new[size+x]=arr2[x];
+    }
+
+    printf("\nMerged Array\n");
+    for(int x=0;x<size+size2;x++){
+        printf("%d  ",new[x]);
+    }
+}
+
 int main(){
+    int data,choice;
+    char option;
+
+    do{
+        printf("Choose one\n1)Insert\n2)Delete\n3)Print\n4)Reverse\n5)Merge\n");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 1:
+            do{
+                printf("Enter data : ");
+                scanf("%d",&data);
+                insert(data);
+                printf("Continue (y/n) :");
+                scanf(" %c",&option);
+            }while(option=='y');
+            break;
+
+            case 2:
+                printf("Enter data : ");
+                scanf("%d",&data);
+                delete(data);
+            break;
+
+            case 3:
+                traverse();
+            break;
+
+            case 4:
+                reverse();
+            break;
+
+            case 5:
+                int arr[3];
+                printf("\nEnter data for second array of size 3\n");
+                scanf("%d%d%d",&arr[0],&arr[1],&arr[2]);
+
+                merge(array,place+1,arr,3);
+            
+        }
+
+        printf("\nReplay program? (y/n): ");
+        scanf(" %c",&option);
+    }while(option=='y');
+
+    return 0;
 
 }
