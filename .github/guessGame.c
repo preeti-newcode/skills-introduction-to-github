ff6a749675204a15dc85baa76e761b7bdcb25bf8
preeti-newcode <preetiraj823@gmail.com>
2025-04-26 15:06:04 +0530

    guessGame.c

--- .github/guessGame.c
+++ .github/guessGame.c
@@ -40,29 +40,94 @@ void display(){
     }
 }
 
-int main(){
-    srand(time(NULL));
+void find(int y){
     int data;
-    int y=rand();
-    while(y>101){
-        y%=101;
-    }
-    printf("%d\n",y);
     do{
         printf("Guess the number : ");
         scanf("%d",&data);
-        if(data == y){
+        if(data == -1){
+            printf("You have chosen to quit this game");
+            exit(1);
+        }
+        else if(data == y){
             printf("Congratulation , YOU WON\n");
             display();
             printf("\nWrong guesses : %d",count);
         }
         else{
-            printf("OH NO!,Try again\n");
+            printf("\tOH NO!,Try again\n");
             insert(data);
             ++count;
+            if(data>y){
+                printf("\tGuess lower value\n");
+            }
+            else{
+                printf("\tGuess higher value\n");
+            }
         }
     }while(data != y);
-    //printf("%d\n",time());
+}
+
+void cleanup(){
+    struct guess *x;
+    while(front != NULL){
+        x=front;
+        front=front->next;
+    }
+}
+
+int main(){
     
+    int y;
+    int level;
+    printf("Welecome in a Guessing Game\n");
+
+    here:
+    printf("Please Choose a level\n");
+    printf("\t1)Easy\n\t2)Medium\n\t3)Hard\n");
+    scanf("%d",&level);
+    switch(level){
+
+        case 1:
+            srand(time(NULL));
+            y=rand();
+            while(y>101){
+                y%=101;
+            }
+            
+            printf("Range 0:100\n");
+            find(y);
+            break;
+        
+        case 2:
+            srand(time(NULL));
+            y=rand();
+            while(y>1001){
+                y%=1001;
+            }
+            
+            printf("Range 0:1000\n");
+            find(y);
+            break;
+            
+        case 3:
+            srand(time(NULL));
+            y=rand();
+            while(y>5001){
+                y%=5001;
+            }
+            
+            printf("Range 0:5000\n");
+            find(y);
+    }
+
+    char again;
+    printf("\nWant to play again (y/n) : ");
+    scanf(" %c",&again);
+
+    cleanup();
+    if(again == 'y'){
+        goto here;
+    }
     return 0;
 }
